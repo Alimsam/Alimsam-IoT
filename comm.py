0ce71fa9e090df3efe9bfa7a54794b79f3cf4c48
junwkdd <junwkdd@gmail.com>
2019-12-26 01:52:28 +0000

    before format

--- comm.py
+++ comm.py
@@ -1,45 +1,35 @@
 import requests, json, time
 from finger import myFinger
+import socket
+import time
 headers = {}
 data = {}
 resCode = ""
-# def resData():
-#     if resCode == "moving" or "outing":
-#         return 1
-#     elif resCode == "register":
-#         return 0
-#     else :
-#         return "No"
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#sock.connect(("54.180.88.152", 3000))
+sock.connect(("10.120.73.120", 3300))
+print('connected to Server')
 
 while 1:
-    print("start")
-    res = requests.get("http://10.120.73.120:3000/finger/fingerStart?method=outing", json=data, headers=headers)
-    print("json:",res.json()['fingerStart'])
-    if str(res.json()['fingerStart']) == "moving" or str(res.json()['fingerStart']) == "outing":
+    print('waiting for ')
+    data_size = 512
+    data = sock.recv(data_size)
+    Situation = str(data).split("'")[1]
+    
+    if Situation == "moving" or Situation == "outing":
         finger = myFinger()
-        finger.searchFinger()
-        Finger = finger.getisFinger()
-        FingerData = finger.getfinData()
-        FingerId = finger.getfinId()
-        if finger == "true":
-            data = {"fingerSuccess":Finger, "fingerData":FingerData, "fingerId":FingerId}
-            
-        elif finger == "false":
-            data = {"fingerSuccess":Finger}
-            
-        elif finger == "add":
-            data = {"fingerSuccess":Finger, "fingerData":FingerData, "fingerId":FingerId}
-    elif str(res.json()['fingerStart']) == "register":
-        finger = myFinger
-        finger.enrollFinger()
-    time.sleep(1)
-# HTTP CODE 
-#print(res.status_code) 
-# # HTTP 응답 원문 
-# print(res.text) 
-# HTTP 요청 값 
-# print(res.request, res.request.body, res.content) 
-# HTTP 응답 원문(JSON)을 디코딩하여 Dictionary로 반환 
-#     if res.json()['fingerStart'] == "moving":
-#     
-#     elif res.json()['fingerStart'] == "outing":
+        isFinger = finger.searchFinger()
+        
+        if isFinger[0] == "true":
+            data = {"fingerSuccess":isFinger[0], "fingerData":isFinger[1], "fingerId":isFinger[2]}
+        elif isFinger[0] == "false":
+            data = {"fingerSuccess":"false"}
+    
+    elif Situation == "register":
+        finger = myFinger()
+        isSuc = finger.enrollFinger()
+        if isSuc[0] == "true":
+            data = {"fingerSuccess":isSuc[0], "fingerData":isSuc[1], "fingerId":isSuc[2]}
+        elif isSuc[0] == "false":
+            data = {"fingerSuccess":"false"}
+    res = requests.get("http://10.120.73.120:3000/"+Situation+"/fingerSuccess", json=data, headers=headers)
--- tcp.py
+++ tcp.py
@@ -1,23 +1,18 @@
 # socket module import!
 import socket
-
+import time
 # socket create and connection
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 #sock.connect(("54.180.88.152", 3000))
 sock.connect(("10.120.73.120", 3300))
 print('connected to Server')
 
-# send msg
-#test_msg = "test"
-#sock.send(test_msg.encode('utf-8'))
-#print('send msg to Server')
-
-# recv data
-print('waiting for ')
-data_size = 512
-data = sock.recv(data_size)
-print('receive data from Server')
-print(data)
-
+while 1:
+    # recv data
+    print('waiting for ')
+    data_size = 512
+    data = sock.recv(data_size)
+    resData = str(data).split("'")[1]
+    print(resData)
 # connection close
 sock.close()
