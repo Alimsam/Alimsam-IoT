10221b91187c25b51f56685221835b0b45ea09a8
junwkdd <junwkdd@gmail.com>
2019-12-30 10:39:11 +0000

    add Json Data -Mr.yang

--- comm.py
+++ comm.py
@@ -26,17 +26,17 @@ while 1:
         finger = myFinger()
         isFinger = finger.searchFinger()
         if isFinger[0] == "true":
-            data = {"situation":Situation,"fingerSuccess":isFinger[0], "fingerId":isFinger[1], "place": place}
+            data = "situation:"+Situation + ",fingerSuccess:"+isFinger[0] + ",fingerId:"+isFinger[1] + ",place:"+place
         elif isFinger[0] == "false":
-            data = {"situation":Situation,"fingerSuccess":"false"}
+            data = "situation:"+Situation + ",fingerSuccess:false"
             
     elif Situation == "outing":
         finger = myFinger()
         isFinger = finger.searchFinger()
         if isFinger[0] == "true":
-            data = {"situation":Situation,"fingerSuccess":isFinger[0], "fingerId":isFinger[1]}
+            data = "situation:"+Situation + ",fingerSuccess:"+isFinger[0] + ",fingerId:"+isFinger[1]
         elif isFinger[0] == "false":
-            data = {"situation":Situation,"fingerSuccess":"false"}
+            data = "situation:"+Situation + ",fingerSuccess:false"
             
     elif Situation == "register":
         name = str(data).split(',')[1]
@@ -44,11 +44,15 @@ while 1:
         finger = myFinger()
         isSuc = finger.enrollFinger()
         if isSuc[0] == "true":
-            data = {"situation":Situation,"fingerSuccess":isSuc[0], "fingerId":isSuc[1], "name": name, "studentId": studentId}
+            data = "situation:"+Situation + ",fingerSuccess:"+isSuc[0] + ",fingerId:"+isSuc[1] + ",name:"+name + ",studentId:"+studentId
+            #data = {"situation":Situation,"fingerSuccess":isSuc[0], "fingerId":isSuc[1], "name": name, "studentId": studentId}
         elif isSuc[0] == "false":
-            data = {"situation":Situation,"fingerSuccess":"false"}
+            data = "situation:"+Situation + ",fingerSuccess:false"
+#             data = {"situation":Situation,"fingerSuccess":"false"}
+        elif isSuc[0] == "already":
+            data = "situation:"+Situation + ",fingerSuccess:"+isSuc[0]
             
-    print("Response json(str) : ",data)
+    print(data)
     sock.send(str(data))
     # res = requests.post("http://" + ip + ":" + str(serverPort) + "/" + Situation + "/fingerSuccess", json=data, headers=headers)
     print("Finish")
